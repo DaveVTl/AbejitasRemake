29f87e10212cfe98f08340f839d1590f38002b2c
cuyrechonchito <lifek523@gmail.com>
2021-11-11 01:44:27 -0500

    error

--- src/main/java/pe/edu/upc/controllers/TipoTrabajoController.java
+++ src/main/java/pe/edu/upc/controllers/TipoTrabajoController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -30,7 +31,7 @@ public class TipoTrabajoController {
 		@Autowired
 		private ITipoTrabajoService cT;
 
-		
+		@Secured({"ROLE_ADMIN"})
 		@GetMapping("/new")
 		public String newTipoT(Model model) {
 			model.addAttribute("tipotrabajo",new TipoTrabajo());
